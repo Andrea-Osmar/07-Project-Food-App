d0393916fa6b9adb0846c693b0921a669fada865
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2020-09-30 10:16:40 +0200

    Finished filter function

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const restaurants = document.getElementsByClassName('restaurant');
+/*** VARIABLES ***/
+
 const cityId = '89';
 const cuisineId = '25';
-// la till två ytterligares sökparametrar som finns i api:et
 const sort = 'rating';
 const order = 'desc';
 const API_URL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&count=10&cuisines=${cuisineId}`;
 const API_URL_SORT = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&count=10&cuisines=${cuisineId}&sort=${sort}&order=${order}`;
-console.log(API_URL_SORT);
 const API_KEY = `43d79a2b3a262a9d099962eba283ced8`;
+
+/*** FUNCTIONS ***/
 
 const request = new Request(API_URL, {
   headers: new Headers({
@@ -16,38 +17,22 @@ const request = new Request(API_URL, {
   }),
 });
 
-const fetchRestaurants = () => {
-  fetch(request) //we use the request object and pass it in to the fetch-function (instead of the URL)
+const fetchRestaurants = (range) => {
+  fetch(request)
     .then((response) => {
       return response.json();
     })
     .then((json) => {
-      console.log(json);
       let restaurantArray = json.restaurants;
-      console.log(restaurantArray);
-      let filteredNewArray = filteredPriceRange(json, 1);
-      console.log(filteredNewArray);
-      filteredNewArray = filteredNewArray.map(restaurantInformation);
-      console.log(filteredNewArray);
-
-      //map
-      const newArray = json.restaurants.map(restaurantInformation);
-
-      // console.log(newArray);
+      let newArray;
+      if (range === 1 || range === 2 || range === 3) {
+        newArray = filteredPriceRange(json, range);
+        newArray = newArray.map(restaurantInformation);
+        document.getElementById('wrapper').innerHTML = '';
+      } else {
+        newArray = json.restaurants.map(restaurantInformation);
+      }
       newArray.forEach(generateHTML);
-
-      newArray.forEach((item, index) => {
-        console.log(restaurants[index]);
-        restaurants[index].querySelector('.rest-name').innerText =
-          item.restName;
-        restaurants[index].querySelector('.rest-address').innerText =
-          item.restAddress;
-        restaurants[index].querySelector('.rest-average-cost').innerText =
-          item.averageCost;
-        restaurants[index].querySelector('.rest-rating').innerText =
-          item.averageRating;
-        restaurants[index].querySelector('.rest-picture').src = item.image;
-      });
     });
 };
 fetchRestaurants();
@@ -65,19 +50,13 @@ const restaurantInformation = (information) => {
 };
 
 const filteredPriceRange = (json, range) => {
-  console.log(json);
-  console.log(range);
   const filteredNewArray = json.restaurants.filter(
     (item) => item.restaurant.price_range === range
   );
-  console.log(filteredNewArray);
   return filteredNewArray;
 };
 
-// la till funktion för att generera HTML. Funkar förutom att bild bara läggs till på den första restaurangen
 const generateHTML = (restaurantArray) => {
-  const imageSrc = restaurantArray.image;
-  console.log(imageSrc);
   let restaurantArticle = `<article class="restaurant">`;
   restaurantArticle += `<div>`;
   restaurantArticle += `<h1 class="rest-name">${restaurantArray.restName}</h1>`;
@@ -85,12 +64,14 @@ const generateHTML = (restaurantArray) => {
   restaurantArticle += `<p class="rest-average-cost">${restaurantArray.averageCost}</p>`;
   restaurantArticle += `<p class="rest-rating">${restaurantArray.averageRating}</p>`;
   restaurantArticle += `</div>`;
-  restaurantArticle += `<div>`;
-  restaurantArticle += `<img class="rest-picture" id="rest-picture" />`;
+  restaurantArticle += `<div class="rest-picture" id="rest-picture">`;
+  //restaurantArticle += `<img class="rest-picture" id="rest-picture" />`;
   restaurantArticle += `</div>`;
   restaurantArticle += `</article>`;
   document.getElementById('wrapper').innerHTML += restaurantArticle;
+  let imageURL = "url('" + restaurantArray.image + "')";
+  console.log(imageURL);
   // console.log(document.getElementById('rest-picture'));
-  document.getElementById('rest-picture').src += imageSrc;
+  document.getElementById('rest-picture').style.backgroundImage = imageURL;
   // console.log(document.getElementById('rest-picture').src);
 };
